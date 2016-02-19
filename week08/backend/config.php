67053ea005007e9996a124731c4895a90716f45f
shiwei89 <1834373693@qq.com>
2016-02-19 11:36:25 +0800

    每个项目目录添加提示文件

---
+++ week08/backend/config.php
@@ -0,0 +1,14 @@
+﻿<?php
+
+header("content-type;text/html;charset=utf-8");
+$con = mysql_connect("localhost","root","");
+if (!$con)
+{
+    die('Could not connect: ' . mysql_error());
+}
+mysql_query("set names utf8");
+
+mysql_select_db("news", $con);
+
+?>
+
